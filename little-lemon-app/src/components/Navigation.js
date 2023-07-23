03ac06799909eb0925b3b33467023ebc7d83ee10
Natalia Torres <torresloranatalia@gmail.com>
2023-07-23 01:36:24 -0400

    Added elements to Navbar

--- little-lemon-app/src/components/Navigation.js
+++ little-lemon-app/src/components/Navigation.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './Logo.svg';
 
 function Nav () {
     return (
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/payment">Planner</Link>
-        <Link to="/confirmation">Get Inspired</Link>
+        <img src={Logo} alt='Logo' width='236px' height = '64px'/>
+        <ul>
+          <li><a href='/home'>Home</a></li>
+          <li><a href='/about'>About</a></li>
+          <li><a href='/menu'>Menu</a></li>
+          <Link to="/">Reservations</Link>
+          <li><a href='/order'>Order Online</a></li>
+          <li><a href='/login'>Login</a></li>
+        </ul>
       </nav>
      )
   }
