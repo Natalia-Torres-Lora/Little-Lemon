e3191f35a54f922bc6770be99ac906c75fed62a8
Natalia Torres <torresloranatalia@gmail.com>
2023-07-23 12:36:27 -0400

    added image to header

--- little-lemon-app/src/components/Header.js
+++ little-lemon-app/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from './restaurant.jpg'
 
 
 function Header () {
     return (
         <>
-        <p>Hello</p>
+        <img src={Image} alt='Restaurant image' width='1440px' height='275px'/>
         </>
      )
   }
