a2aa66eeee15a967d1f7526bab2cd5335e03e6c6
Natalia Torres <torresloranatalia@gmail.com>
2023-07-23 14:30:15 -0400

    Added footer elements

--- little-lemon-app/src/components/Footer.js
+++ little-lemon-app/src/components/Footer.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Image from './footer-logo.png'
 
-function Footer () {
+const Footer = ()=> {
     return (
-        <>
-        <p>Hello</p>
-        </>
-     )
-  }
+        <footer>
+        <div className='container'>
+            <div className='footer-logo'>
+                <img src={Image} alt='Little Lemon Logo' width="112px" height="158px"/>
+            </div>
+            <nav>
+                <h6>Navigation</h6>
+                <ul>
+                    <li><a href='/home'>Home</a></li>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/menu'>Menu</a></li>
+                    <Link to="/">Reservations</Link>
+                    <li><a href='/order'>Order Online</a></li>
+                    <li><a href='/login'>Login</a></li>
+                </ul>
+            </nav>
+            <div className='footer-contact'>
+                <p>123 Lemon Street, Chicago, US</p>
+                <p>+1 787-123-4567</p>
+                <p>info@lemon.com</p>
+            </div>
+            <div className='socialmedia-links'>
+                <h6>Social Media</h6>
+                <ul>
+                    <li><a href="https://www.instagram.com/littlelemon" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                    <li><a href="https://www.facebook.com/littlelemon" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li><a href="https://www.twitter.com/littlelemon" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+                </ul>
+            </div>
+        </div>
+        </footer>
+     );
+  };
   export default Footer;
--- little-lemon-app/src/components/Header.js
+++ little-lemon-app/src/components/Header.js
@@ -5,7 +5,13 @@ import Image from './restaurant.jpg'
 function Header () {
     return (
         <>
-        <img src={Image} alt='Restaurant image' width='1440px' height='275px'/>
+        <div>
+            <h1>Little Lemon</h1>
+            <h3>Chicago</h3>
+        </div>
+        <div>
+            <img src={Image} alt='Restaurant image' width='1440px' height='275px'/>
+        </div>
         </>
      )
   }
