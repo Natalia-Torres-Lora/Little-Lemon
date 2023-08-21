f71d8e54ec48762bdcd7c078735512b1ba5c165a
Natalia Torres <torresloranatalia@gmail.com>
2023-08-21 09:58:31 -0400

    Added

--- little-lemon-app/src/pages/ReservationPage.js
+++ little-lemon-app/src/pages/ReservationPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Arrow from './Images/ArrowLeftCircle.svg'
 
 
 function ReservationPage () {
     return (
-        <>
-        <p>Hello</p>
-        </>
+        <main>
+            <div className='prev-page-logo'>
+                <a href='little-lemon/home'>
+                    <img src={Arrow} alt='Previus Page Button'/>
+                </a>
+            </div>
+            <h2>Reserve your Table</h2>
+            
+        </main>
      )
   }
   export default ReservationPage;
