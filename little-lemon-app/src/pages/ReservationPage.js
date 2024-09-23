38873a6843f87b91ae1c5f924e0e36861c61461d
Natalia Torres <torresloranatalia@gmail.com>
2024-09-23 19:11:56 -0400

    Added the form components for the reservation page

--- little-lemon-app/src/pages/ReservationPage.js
+++ little-lemon-app/src/pages/ReservationPage.js
@@ -1,10 +1,14 @@
 //ReservationPage.js
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonComponent from '../components/ButtonComponent';
 import Arrow from './Images/ArrowLeftCircle.svg'
 
 
 function ReservationPage () {
+//elements and state management to store the user's input.
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+
     return (
         <main>
             <div className='prev-page-logo'>
@@ -14,6 +18,49 @@ function ReservationPage () {
             </div>
             <div>
                 <h2>Reserve Your Table</h2>
+                <form>
+                    <label htmlFor="date">Date:</label>
+                    <input
+                    type='date'
+                    id='date'
+                    name='date'
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    />
+                    <label htmlFor='time'>Time:</label>
+                    <input
+                        type="time"
+                        id="time"
+                        name="time"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                    />
+                    <label htmlFor='guests'>Guests</label>
+                    <select id='guests' name='guests'>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6'>6</option>
+                    </select>
+                    <label htmlFor='occasion'>Occasion</label>
+                    <select id='occasion' name='occasion'>
+                        <option value='birthday'>Birthday</option>
+                        <option value='anniversary'>Anniversary</option>
+                        <option value='other'>Other</option>
+                    </select>
+                    <fieldset>
+  <legend>Choose your seating option:</legend>
+  <label>
+    <input type="radio" name="seating" value="outdoor" /> Outdoor
+  </label>
+  <br></br>
+  <label>
+    <input type="radio" name="seating" value="indoor" /> Indoor
+  </label>
+</fieldset>
+                </form>
                 <h4>Special Instructions</h4>
             </div>
             <div>
